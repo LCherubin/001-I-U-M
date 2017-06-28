679196479c3f65b2e30f445baa207b64f53ac883
LCherubin <lcherubin7@gmail.com>
2017-06-28 14:09:03 +0100

    Gestion
    I-U-M

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { MathFinEcoSharedModule, UserRouteAccessService } from './shared';
+import { MathFinEcoHomeModule } from './home/home.module';
+import { MathFinEcoAdminModule } from './admin/admin.module';
+import { MathFinEcoAccountModule } from './account/account.module';
+import { MathFinEcoEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        MathFinEcoSharedModule,
+        MathFinEcoHomeModule,
+        MathFinEcoAdminModule,
+        MathFinEcoAccountModule,
+        MathFinEcoEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class MathFinEcoAppModule {}
